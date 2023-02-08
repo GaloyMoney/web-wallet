d06109ab9ef6053776e42ac5c0ca61840180764c
ntheile <ntheile@gmail.com>
2023-02-07 20:51:34 -0600

    fix: clear cookies on auth error

--- src/components/auth-provider.tsx
+++ src/components/auth-provider.tsx
@@ -86,6 +86,15 @@ export const AuthProvider: FCT = ({ children, galoyClient, authIdentity }) => {
     }
     return createClient({
       onError: ({ graphQLErrors, networkError }) => {
+        if (networkError && networkError.message.includes("Failed to fetch")) {
+          fetch(config.galoyAuthEndpoint + "/clearCookies", {
+            method: 'GET',
+            redirect: 'follow',
+            credentials: 'include'
+          })
+          localStorage.clear()
+          location.href = "/"
+        }
         if (graphQLErrors) {
           console.debug("[GraphQL errors]:", graphQLErrors)
         }

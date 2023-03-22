fcbeaaf2033a9f5d561368f11f310c853c42bf90
ntheile <ntheile@gmail.com>
2023-03-22 00:01:47 -0500

    feat: use query hook example from codegen

--- src/components/pages/home.tsx
+++ src/components/pages/home.tsx
@@ -2,14 +2,37 @@ import { translate, useAuthContext, NoPropsFCT } from "store/index"
 
 import Header from "components/header"
 import TransactionList from "components/transactions/list"
+import { gql } from "@apollo/client"
+import { useBtcPriceListQuery } from "graphql/generated"
+
+gql`
+  query btcPriceList($range: PriceGraphRange!) {
+    btcPriceList(range: $range) {
+      timestamp
+      price {
+        base
+        offset
+        currencyUnit
+      }
+    }
+  }
+`
 
 const Home: NoPropsFCT = () => {
   const { isAuthenticated } = useAuthContext()
+
+  const { error, data } = useBtcPriceListQuery({
+    fetchPolicy: "no-cache",
+    variables: { range: "ONE_DAY" },
+  })
 
   return (
     <>
       <div className="home">
         <Header page="home" />
+
+        <p>{JSON.stringify(data?.btcPriceList)}</p>
+        <p>{error && error.message}</p>
 
         <div className="recent-transactions">
           {isAuthenticated && (

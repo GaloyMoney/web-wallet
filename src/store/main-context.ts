505653d2e72cea3052a8bf0b314c82d97b301566
ntheile <ntheile@gmail.com>
2023-07-02 19:03:00 -0500

    feat: use new dedicated websocket server

--- src/store/main-context.ts
+++ src/store/main-context.ts
@@ -1,13 +1,95 @@
 import { createContext, useContext } from "react"
-
-import {
-  createGaloyClient,
-  createGaloyServerAdminClient,
-  createGaloyServerClient,
-} from "@galoymoney/client"
-
-import { config } from "store/config"
+import { config as storeConfig } from "store/config"
 import { GwwActionType, GwwContextType } from "store/index"
+import {
+  ApolloClient,
+  ApolloLink,
+  HttpLink,
+  InMemoryCache,
+  split,
+  from,
+  NormalizedCacheObject,
+} from "@apollo/client"
+import { ErrorLink, onError } from "@apollo/client/link/error"
+import { GraphQLWsLink } from "@apollo/client/link/subscriptions"
+
+import { createClient as createWsClient } from "graphql-ws"
+import { getMainDefinition } from "@apollo/client/utilities"
+
+const defaultErrorCallback: ErrorLink.ErrorHandler = ({
+  graphQLErrors,
+  networkError,
+}) => {
+  if (graphQLErrors) {
+    console.debug("[GraphQL errors]:", graphQLErrors)
+  }
+  if (networkError) {
+    console.debug("[Network error]:", networkError)
+  }
+}
+
+export type CreateGaloyClientFunction = (argW: {
+  config: Record<string, any>
+  initData?: NormalizedCacheObject
+}) => (arg?: {
+  authToken?: string
+  headers?: Record<string, string | Array<string> | undefined>
+  onError?: ErrorLink.ErrorHandler
+}) => ApolloClient<NormalizedCacheObject>
+
+export const createGaloyClient: CreateGaloyClientFunction =
+  ({ config, initData }) =>
+  ({ authToken, onError: onErrorCallback } = {}) => {
+    const cache = initData ? new InMemoryCache().restore(initData) : new InMemoryCache()
+    const errorLink = onError(onErrorCallback ?? defaultErrorCallback)
+
+    const authLink = new ApolloLink((operation, forward) => {
+      operation.setContext(({ headers }: { headers: Record<string, string> }) => ({
+        headers: {
+          authorization: authToken ? `Bearer ${authToken}` : "",
+          ...headers,
+          Cookie: document.cookie,
+        },
+      }))
+      return forward(operation)
+    })
+
+    const httpLink = new HttpLink({ uri: config.graphqlUrl, credentials: "include" })
+
+    const wsLink = new GraphQLWsLink(
+      createWsClient({
+        url: config.graphqlSubscriptionUrl,
+        retryAttempts: 3,
+        lazy: true,
+        connectionParams: async () => {
+          return {
+            credentials: "include",
+          }
+        },
+        shouldRetry: (errOrCloseEvent) => {
+          console.warn({ errOrCloseEvent }, "entering shouldRetry function for websocket")
+          return true
+        },
+      }),
+    )
+
+    const splitLink = split(
+      ({ query }) => {
+        const definition = getMainDefinition(query)
+        return (
+          definition.kind === "OperationDefinition" &&
+          definition.operation === "subscription"
+        )
+      },
+      wsLink,
+      from([errorLink, authLink, httpLink]),
+    )
+
+    return new ApolloClient({
+      cache,
+      link: splitLink,
+    })
+  }
 
 export const GwwContext = createContext<GwwContextType>({
   state: { key: 0, path: "/" },
@@ -26,8 +108,4 @@ export const useAppDispatcher = () => {
   return dispatch
 }
 
-export const createClient = config.isBrowser
-  ? createGaloyClient({ config })
-  : createGaloyServerClient({ config })
-
-export const createAdminClient = createGaloyServerAdminClient({ config })
+export const createClient = createGaloyClient({ config: storeConfig })
